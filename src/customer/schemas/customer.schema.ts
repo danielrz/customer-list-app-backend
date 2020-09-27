585706fa5edd638e2e0bd561f6c71b5b0e8f42f5
danielr <danielr@liveperson.com>
2020-09-27 15:07:06 +0300

    working version without test

---
+++ src/customer/schemas/customer.schema.ts
@@ -0,0 +1,15 @@
+import { MongooseModule } from "@nestjs/mongoose";
+
+import * as mongoose from 'mongoose';
+export const CustomerSchema = new mongoose.Schema({
+    first_name: String,
+    last_name: String,
+    email: String,
+    phone: String,
+    address: String,
+    description: String,
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+});
